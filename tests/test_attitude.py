faa365b0305def3644bb75ccfe35fd38167fd555
Vegard R. Solum <vegard.solum@4subsea.com>
2025-11-12 09:16:32 +0100

    delete old euler code

--- tests/test_attitude.py
+++ tests/test_attitude.py
@@ -27,25 +27,3 @@ class Test_UnitQuaternion:
         euler = np.array([0.0, 0.0, 0.0])
         q = UnitQuaternion.from_euler(euler)
         np.testing.assert_allclose(q.toarray(), [1.0, 0.0, 0.0, 0.0])
-
-
-# class Test_EulerZYX:
-#     def test__init__(self):
-#         theta = np.array([np.pi / 8, np.pi / 4, np.pi / 2])
-#         euler = EulerZYX(theta)
-#         np.testing.assert_allclose(euler.toarray(), np.array(theta))
-
-#     def test__init__radians(self):
-#         theta = np.array([np.pi / 8, np.pi / 4, np.pi / 2])
-#         euler = EulerZYX(theta, degrees=False)
-#         np.testing.assert_allclose(euler.toarray(), np.array(theta))
-
-#     def test__init__degrees(self):
-#         theta = [10.0, 20.0, 30.0]
-#         euler = EulerZYX(theta, degrees=True)
-#         np.testing.assert_allclose(euler.toarray(), (np.pi / 180.0) * np.array(theta))
-
-#     def test_from_quaternion(self):
-#         q = [1.0, 0.0, 0.0, 0.0]
-#         euler = EulerZYX.from_quaternion(q)
-#         np.testing.assert_allclose(euler.toarray(), np.array([0.0, 0.0, 0.0]))
