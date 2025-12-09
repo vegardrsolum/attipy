e93fd1de421ad3f38f9a0d0a1bf33b356c248042
Vegard R. Solum <vegard.solum@4subsea.com>
2025-12-09 14:42:03 +0100

    docstring

--- src/attipy/_attitude.py
+++ src/attipy/_attitude.py
@@ -83,8 +83,27 @@ class UnitQuaternion:
 
     def as_matrix(self) -> np.ndarray:
         """
-        Return the attitude as a rotation matrix.
+        Return the attitude as a rotation matrix (or direction cosine matrix), A,
+        defined such that it transforms vectors from the body frame to the navigation
+        frame using:
 
-        
+            v_n = A @ v_b
+
+        where,
+        - v_b is a vector expressed in the body frame.
+        - v_n is the same vector expressed in the navigation frame.
+
+        The rotation matrix is computed from the unit quaternion using the formula:
+
+            A = I + 2 * q_w * S(q_xyz) + 2 * S(q_xyz)^2
+
+        where,
+
+        - I is the 3x3 identity matrix.
+        - q_w is the scalar part of the unit quaternion.
+        - q_xyz is the vector part, [q_x, q_y, q_z], of the unit quaternion.
+        - S(q_xyz) is the skew-symmetric matrix of q_xyz.
+
+
         """
         return _matrix_from_quaternion(self._q)
