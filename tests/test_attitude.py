943bbb9ceaf08111e7bbb6506bae6117a6831275
Vegard R. Solum <vegard.solum@4subsea.com>
2025-12-10 08:35:57 +0100

    more tests

--- tests/test_attitude.py
+++ tests/test_attitude.py
@@ -6,9 +6,30 @@ from attipy import Attitude
 
 
 class Test_Attitude:
+    euler_deg_data = [
+        (0.0, 0.0, 0.0),
+        (10.0, 0.0, 0.0),
+        (0.0, 10.0, 0.0),
+        (0.0, 0.0, 10.0),
+        (1.0, 2.0, 3.0),
+        (-1.0, -2.0, -3.0),
+        (90.0, 25.0, -130.0),
+    ]
+
     def test__init__(self):
         att = Attitude([1.0, 0.0, 0.0, 0.0])
         np.testing.assert_allclose(att._q, [1.0, 0.0, 0.0, 0.0])
+
+    @pytest.mark.parametrize("euler_deg", euler_deg_data)
+    def test_from_quaternion(self, euler_deg):
+        # Use scipy as reference
+        R = Rotation.from_euler("ZYX", euler_deg[::-1], degrees=True)
+        q = R.as_quat(scalar_first=True)
+
+        att = Attitude.from_quaternion(q)
+        q_out = att._q
+        q_expect = np.asarray(q, dtype=float)
+        np.testing.assert_allclose(q_out, q_expect)
 
     @pytest.mark.parametrize(
         "euler_deg",
