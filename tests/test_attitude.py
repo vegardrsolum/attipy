bda11441ce7e768566f08ce634d6ee3635e746fc
Vegard R. Solum <vegard.solum@4subsea.com>
2025-12-10 08:41:50 +0100

    reuse euler_deg_data

--- tests/test_attitude.py
+++ tests/test_attitude.py
@@ -31,78 +31,43 @@ class Test_Attitude:
         q_expect = np.asarray(q, dtype=float)
         np.testing.assert_allclose(q_out, q_expect)
 
-    @pytest.mark.parametrize(
-        "euler_deg",
-        [
-            (0.0, 0.0, 0.0),
-            (10.0, 0.0, 0.0),
-            (0.0, 10.0, 0.0),
-            (0.0, 0.0, 10.0),
-            (1.0, 2.0, 3.0),
-            (-1.0, -2.0, -3.0),
-            (90.0, 25.0, -130.0),
-        ],
-    )
+    @pytest.mark.parametrize("euler_deg", euler_deg_data)
     def test_from_euler_deg(self, euler_deg):
-        att = Attitude.from_euler(euler_deg, degrees=True)
-        q_out = att._q
-        q_expect = Rotation.from_euler("ZYX", euler_deg[::-1], degrees=True).as_quat(
-            scalar_first=True
-        )
-        np.testing.assert_allclose(q_out, q_expect)
+        # Use scipy as reference
+        R = Rotation.from_euler("ZYX", euler_deg[::-1], degrees=True)
+        q = R.as_quat(scalar_first=True)
 
-    @pytest.mark.parametrize(
-        "euler_deg",
-        [
-            (0.0, 0.0, 0.0),
-            (10.0, 0.0, 0.0),
-            (0.0, 10.0, 0.0),
-            (0.0, 0.0, 10.0),
-            (1.0, 2.0, 3.0),
-            (-1.0, -2.0, -3.0),
-            (90.0, 25.0, -130.0),
-        ],
-    )
+        att = Attitude.from_euler(euler_deg, degrees=True)
+
+        np.testing.assert_allclose(att._q, q)
+
+    @pytest.mark.parametrize("euler_deg", euler_deg_data)
     def test_from_euler_rad(self, euler_deg):
+        # Use scipy as reference
+        R = Rotation.from_euler("ZYX", euler_deg[::-1], degrees=True)
+        q = R.as_quat(scalar_first=True)
+
         euler_rad = np.radians(euler_deg)
         att = Attitude.from_euler(euler_rad, degrees=False)
-        q_out = att._q
-        q_expect = Rotation.from_euler("ZYX", euler_rad[::-1], degrees=False).as_quat(
-            scalar_first=True
-        )
-        np.testing.assert_allclose(q_out, q_expect)
 
-    @pytest.mark.parametrize(
-        "euler_deg",
-        [
-            (0.0, 0.0, 0.0),
-            (10.0, 0.0, 0.0),
-            (0.0, 10.0, 0.0),
-            (0.0, 0.0, 10.0),
-            (1.0, 2.0, 3.0),
-            (-1.0, -2.0, -3.0),
-            (90.0, 25.0, -130.0),
-        ],
-    )
+        np.testing.assert_allclose(att._q, q)
+
+    @pytest.mark.parametrize("euler_deg", euler_deg_data)
     def test_as_euler_deg(self, euler_deg):
-        att = Attitude.from_euler(euler_deg, degrees=True)
+        # Use scipy as reference
+        R = Rotation.from_euler("ZYX", euler_deg[::-1], degrees=True)
+        q = R.as_quat(scalar_first=True)
+
+        att = Attitude(q)
         euler_out = att.as_euler(degrees=True)
         np.testing.assert_allclose(euler_out, euler_deg)
 
-    @pytest.mark.parametrize(
-        "euler_deg",
-        [
-            (0.0, 0.0, 0.0),
-            (10.0, 0.0, 0.0),
-            (0.0, 10.0, 0.0),
-            (0.0, 0.0, 10.0),
-            (1.0, 2.0, 3.0),
-            (-1.0, -2.0, -3.0),
-            (90.0, 25.0, -130.0),
-        ],
-    )
+    @pytest.mark.parametrize("euler_deg", euler_deg_data)
     def test_as_euler_rad(self, euler_deg):
-        euler_rad = np.radians(euler_deg)
-        att = Attitude.from_euler(euler_deg, degrees=True)
+        # Use scipy as reference
+        R = Rotation.from_euler("ZYX", euler_deg[::-1], degrees=True)
+        q = R.as_quat(scalar_first=True)
+
+        att = Attitude(q)
         euler_out = att.as_euler(degrees=False)
-        np.testing.assert_allclose(euler_out, euler_rad)
+        np.testing.assert_allclose(euler_out, np.radians(euler_deg))
