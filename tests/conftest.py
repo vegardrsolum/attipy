dd69afd396f9cfa3c600836eb3b1c07a510f9e12
Vegard R. Solum <vegard.solum@4subsea.com>
2025-12-17 08:37:58 +0100

    small fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,35 @@
 from pathlib import Path
 
 import pytest
+import numpy as np
 
 TEST_PATH = Path(__file__).parent
 
 
 @pytest.fixture
 def ahrs_data():
-    # TODO: remove pandas dependency
-    import pandas as pd
+    data = np.genfromtxt(
+        TEST_PATH / "testdata/benchmark_pure_attitude_beat_202311A.csv",
+        delimiter=",",
+        names=True,
+        dtype=float,
+    )
 
-    df = pd.read_csv(TEST_PATH / r"testdata/benchmark_pure_attitude_beat_202311A.csv")
-
-    t = df["Time_s"].values.astype(float)
-    euler = df[["Roll_rad", "Pitch_rad", "Yaw_rad"]].values.astype(float)
-    w = df[["GyroX_rads", "GyroY_rads", "GyroZ_rads"]].values.astype(float)
-    f = df[["AccX_ms2", "AccY_ms2", "AccZ_ms2"]].values.astype(float)
+    t = data["Time_s"]
+    euler = np.column_stack((
+        data["Roll_rad"],
+        data["Pitch_rad"],
+        data["Yaw_rad"],
+    ))
+    w = np.column_stack((
+        data["GyroX_rads"],
+        data["GyroY_rads"],
+        data["GyroZ_rads"],
+    ))
+    f = np.column_stack((
+        data["AccX_ms2"],
+        data["AccY_ms2"],
+        data["AccZ_ms2"],
+    ))
 
     return t, euler, f, w
