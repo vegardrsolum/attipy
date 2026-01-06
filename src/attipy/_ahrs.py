07b7a803584c1876d0ca557096341f0c9f137df3
Vegard R. Solum <vegard.solum@4subsea.com>
2026-01-06 11:07:59 +0100

    small fix

--- src/attipy/_ahrs.py
+++ src/attipy/_ahrs.py
@@ -349,10 +349,8 @@ class AHRS:
         if not dx.any():
             return
 
-        # Quaternion error
         da = dx[0:3]
         self._dq[:] = np.array([2.0, *da]) / np.sqrt(4.0 + da.T @ da)
-
         self._att._q[:] = _normalize(_quatprod(self._att._q, self._dq))
         self._bg[:] = self._bg + dx[3:6]
         self._v[:] = self._v + dx[6:9]
