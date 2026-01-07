245402dfd518f679cf78c11437c75b1cb4baa31f
Vegard R. Solum <vegard.solum@4subsea.com>
2026-01-07 22:43:30 +0100

    phases refactor

--- src/attipy/_simulate.py
+++ src/attipy/_simulate.py
@@ -535,13 +535,15 @@ def _beating_sim(g, nav_frame):
     f_main, f_beat = 0.1, 0.01
     amp_p, amp_r = 0.5, np.radians(5.0)
 
+    phases = (0.0, np.pi / 3, 2 * np.pi / 3, np.pi, 4 * np.pi / 3, 5 * np.pi / 3)
+
     sim = PVASimulator(
-        pos_x=BeatDOF(amp_p, f_main, f_beat, freq_hz=True, phase=0.0),
-        pos_y=BeatDOF(amp_p, f_main, f_beat, freq_hz=True, phase=np.pi / 3),
-        pos_z=BeatDOF(amp_p, f_main, f_beat, freq_hz=True, phase=2 * np.pi / 3),
-        alpha=BeatDOF(amp_r, f_main, f_beat, freq_hz=True, phase=np.pi),
-        beta=BeatDOF(amp_r, f_main, f_beat, freq_hz=True, phase=4 * np.pi / 3),
-        gamma=BeatDOF(amp_r, f_main, f_beat, freq_hz=True, phase=5 * np.pi / 3),
+        pos_x=BeatDOF(amp_p, f_main, f_beat, freq_hz=True, phase=phases[0]),
+        pos_y=BeatDOF(amp_p, f_main, f_beat, freq_hz=True, phase=phases[1]),
+        pos_z=BeatDOF(amp_p, f_main, f_beat, freq_hz=True, phase=phases[2]),
+        alpha=BeatDOF(amp_r, f_main, f_beat, freq_hz=True, phase=phases[3]),
+        beta=BeatDOF(amp_r, f_main, f_beat, freq_hz=True, phase=phases[4]),
+        gamma=BeatDOF(amp_r, f_main, f_beat, freq_hz=True, phase=phases[5]),
         degrees=False,
         g=g,
         nav_frame=nav_frame,
@@ -555,13 +557,15 @@ def _chirp_sim(g, nav_frame):
     f_max, f_os = 0.25, 0.01
     amp_p, amp_r = 0.5, np.radians(5.0)
 
+    phases = (0.0, np.pi / 3, 2 * np.pi / 3, np.pi, 4 * np.pi / 3, 5 * np.pi / 3)
+
     sim = PVASimulator(
-        pos_x=ChirpDOF(amp_p, f_max, f_os, freq_hz=True, phase=0.0),
-        pos_y=ChirpDOF(amp_p, f_max, f_os, freq_hz=True, phase=np.pi / 3),
-        pos_z=ChirpDOF(amp_p, f_max, f_os, freq_hz=True, phase=2 * np.pi / 3),
-        alpha=ChirpDOF(amp_r, f_max, f_os, freq_hz=True, phase=np.pi),
-        beta=ChirpDOF(amp_r, f_max, f_os, freq_hz=True, phase=4 * np.pi / 3),
-        gamma=ChirpDOF(amp_r, f_max, f_os, freq_hz=True, phase=5 * np.pi / 3),
+        pos_x=ChirpDOF(amp_p, f_max, f_os, freq_hz=True, phase=phases[0]),
+        pos_y=ChirpDOF(amp_p, f_max, f_os, freq_hz=True, phase=phases[1]),
+        pos_z=ChirpDOF(amp_p, f_max, f_os, freq_hz=True, phase=phases[2]),
+        alpha=ChirpDOF(amp_r, f_max, f_os, freq_hz=True, phase=phases[3]),
+        beta=ChirpDOF(amp_r, f_max, f_os, freq_hz=True, phase=phases[4]),
+        gamma=ChirpDOF(amp_r, f_max, f_os, freq_hz=True, phase=phases[5]),
         degrees=False,
         g=g,
         nav_frame=nav_frame,
