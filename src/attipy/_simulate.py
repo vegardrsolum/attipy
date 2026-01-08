7e8b6d3cbc294be5885f8c2a604a56345bf3f7a0
Vegard R. Solum <vegard.solum@4subsea.com>
2026-01-08 16:29:50 +0100

    delete old commented out code

--- src/attipy/_simulate.py
+++ src/attipy/_simulate.py
@@ -530,28 +530,6 @@ class PVASimulator:
         return t, pos, vel, euler, f_b, w_b
 
 
-# def _beating_sim(g, nav_frame):
-#     """Create a beating PVA simulator."""
-#     f_main, f_beat = 0.1, 0.01
-#     amp_p, amp_r = 0.5, np.radians(5.0)
-
-#     phases = (0.0, np.pi / 3, 2 * np.pi / 3, np.pi, 4 * np.pi / 3, 5 * np.pi / 3)
-
-#     sim = PVASimulator(
-#         pos_x=BeatDOF(amp_p, f_main, f_beat, freq_hz=True, phase=phases[0]),
-#         pos_y=BeatDOF(amp_p, f_main, f_beat, freq_hz=True, phase=phases[1]),
-#         pos_z=BeatDOF(amp_p, f_main, f_beat, freq_hz=True, phase=phases[2]),
-#         alpha=BeatDOF(amp_r, f_main, f_beat, freq_hz=True, phase=phases[3]),
-#         beta=BeatDOF(amp_r, f_main, f_beat, freq_hz=True, phase=phases[4]),
-#         gamma=BeatDOF(amp_r, f_main, f_beat, freq_hz=True, phase=phases[5]),
-#         degrees=False,
-#         g=g,
-#         nav_frame=nav_frame,
-#     )
-
-#     return sim
-
-
 def _beating_pva_sim(g, nav_frame):
     """Create a beating PVA simulator."""
     f_main, f_beat = 0.1, 0.01
@@ -636,28 +614,6 @@ def _chirp_att_sim(g, nav_frame):
     )
 
     return sim
-
-
-# def _chirp_sim(g, nav_frame):
-#     """Create a beating PVA simulator."""
-#     f_max, f_os = 0.25, 0.01
-#     amp_p, amp_r = 0.5, np.radians(5.0)
-
-#     phases = (0.0, np.pi / 3, 2 * np.pi / 3, np.pi, 4 * np.pi / 3, 5 * np.pi / 3)
-
-#     sim = PVASimulator(
-#         pos_x=ChirpDOF(amp_p, f_max, f_os, freq_hz=True, phase=phases[0]),
-#         pos_y=ChirpDOF(amp_p, f_max, f_os, freq_hz=True, phase=phases[1]),
-#         pos_z=ChirpDOF(amp_p, f_max, f_os, freq_hz=True, phase=phases[2]),
-#         alpha=ChirpDOF(amp_r, f_max, f_os, freq_hz=True, phase=phases[3]),
-#         beta=ChirpDOF(amp_r, f_max, f_os, freq_hz=True, phase=phases[4]),
-#         gamma=ChirpDOF(amp_r, f_max, f_os, freq_hz=True, phase=phases[5]),
-#         degrees=False,
-#         g=g,
-#         nav_frame=nav_frame,
-#     )
-
-#     return sim
 
 
 def pva_data(
