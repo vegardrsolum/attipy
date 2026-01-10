4402308dbd0d1668eba48b661fdb2eed8eb20f08
Vegard R. Solum <vegard.solum@4subsea.com>
2026-01-10 15:04:59 +0100

    test

--- tests/test_statespace.py
+++ tests/test_statespace.py
@@ -1,7 +1,29 @@
 import numpy as np
 
 import attipy as ap
+from attipy._vectorops import _skew_symmetric
 
+
+def test_state_matrix():
+    f_b_corr = np.array([0.1, 0.2, 9.7])
+    w_b_corr = np.array([0.01, 0.02, 0.03])
+    R_nb = ap.Attitude.from_euler([0.1, 0.2, 0.3]).as_matrix()
+    gbc = 0.04
+
+    dfdx = ap._statespace._state_matrix(f_b_corr, w_b_corr, R_nb, gbc)
+
+    S = _skew_symmetric  # alias skew symmetric matrix
+
+    beta_gyro = 1.0 / gbc
+
+    # Linearized state matrix
+    dfdx_expect = np.zeros((9, 9))
+    dfdx_expect[0:3, 0:3] = -S(w_b_corr)
+    dfdx_expect[0:3, 3:6] = -np.eye(3)
+    dfdx_expect[3:6, 3:6] = -beta_gyro * np.eye(3)
+    dfdx_expect[6:9, 0:3] = -R_nb @ S(f_b_corr)
+
+    assert np.allclose(dfdx, dfdx_expect)
 
 def test_state_transition():
     dt = 0.1
@@ -16,4 +38,4 @@ def test_state_transition():
     # First order approximation
     phi_approx = np.eye(9) + dt * dfdx
 
-    assert np.allclose(phi, phi_approx, atol=1e-8)
+    assert np.allclose(phi, phi_approx)
