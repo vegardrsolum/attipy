89853a69fcc2eeee5c98608544505fe8e0b0c68b
Vegard R. Solum <vegard.solum@4subsea.com>
2026-01-10 14:20:22 +0100

    refactor with statespace file

--- src/attipy/_ahrs.py
+++ src/attipy/_ahrs.py
@@ -6,7 +6,10 @@ from numpy.typing import ArrayLike, NDArray
 
 from ._attitude import Attitude
 from ._quatops import _quatprod
-from ._vectorops import _normalize, _skew_symmetric
+from ._statespace import _state_transition_matrix
+from ._statespace import _update_state_transition_matrix as _update_phi
+from ._statespace import _wn_cov_matrix
+from ._vectorops import _normalize
 
 
 def _gravity_nav(g, nav_frame) -> NDArray[np.float64]:
@@ -40,110 +43,6 @@ def _ssa(angle: float, degrees: bool = True) -> float:
     """
     base = 180.0 if degrees else np.pi
     return (angle + base) % (2.0 * base) - base
-
-
-def _state_matrix(f_b_corr, w_b_corr, R_nb, gbc) -> NDArray[np.float64]:
-    """Setup linearized state matrix, dfdx."""
-
-    beta_gyro = 1.0 / gbc
-
-    S = _skew_symmetric  # alias skew symmetric matrix
-
-    # State transition matrix
-    dfdx = np.zeros((9, 9))
-    dfdx[0:3, 0:3] = -S(w_b_corr)  # NB! update each time step
-    dfdx[0:3, 3:6] = -np.eye(3)
-    dfdx[3:6, 3:6] = -beta_gyro * np.eye(3)
-    dfdx[6:9, 0:3] = -R_nb @ S(f_b_corr)  # NB! update each time step
-
-    return dfdx
-
-
-# def _state_transition_matrix_old(
-#     dt, f_b_corr, w_b_corr, R_nb, gbc
-# ) -> NDArray[np.float64]:
-#     """Setup state transition matrix, phi."""
-#     dfdx = _state_matrix(f_b_corr, w_b_corr, R_nb, gbc)
-#     phi = np.eye(9) + dt * dfdx  # first-order approximation
-#     return phi
-
-
-def _state_transition_matrix(dt, f_b_corr, w_b_corr, R_nb, gbc) -> NDArray[np.float64]:
-    """
-    Setup state transition matrix, phi.
-
-    First order approximation:
-        phi = I + dt * dfdx
-    """
-    S = _skew_symmetric  # alias skew symmetric matrix
-
-    beta_gyro = 1.0 / gbc
-
-    phi = np.eye(9)
-    phi[0:3, 0:3] += -dt * S(w_b_corr)
-    phi[0:3, 3:6] += -dt * np.eye(3)
-    phi[3:6, 3:6] += -dt * beta_gyro * np.eye(3)  # => (1 - dt*beta_gyro) * I
-    phi[6:9, 0:3] += -dt * (R_nb @ S(f_b_corr))
-    return phi
-
-
-@njit  # type: ignore[misc]
-def _update_phi(phi, dt, I3x3, f_b, w_b, R_nb):
-    """Update state transition matrix, phi."""
-    S = _skew_symmetric
-    phi[0:3, 0:3] = I3x3 - dt * S(w_b)
-    phi[6:9, 0:3] = -dt * R_nb @ S(f_b)
-
-
-# def _wn_input_matrix(R_nb: NDArray[np.float64]) -> NDArray[np.float64]:
-#     """Setup linearized (white noise) input matrix, dfdw."""
-
-#     # Input (white noise) matrix
-#     dfdw = np.zeros((9, 9))
-#     dfdw[0:3, 0:3] = -np.eye(3)
-#     dfdw[3:6, 3:6] = np.eye(3)
-#     dfdw[6:9, 6:9] = -R_nb  # NB! update each time step (for non-isotropic case)
-
-#     return dfdw
-
-
-# def _wn_psd_matrix(
-#     vrw: float, arw: float, gbs: float, gbc: float
-# ) -> NDArray[np.float64]:
-#     """Setup white noise (process noise) power spectral density matrix, W."""
-
-#     # White noise power spectral density matrix
-#     W = np.eye(9)
-#     W[0:3, 0:3] *= arw**2
-#     W[3:6, 3:6] *= 2.0 * gbs**2 / gbc
-#     W[6:9, 6:9] *= vrw**2
-
-#     return W
-
-
-# def _wn_cov_matrix_old(dt, R_nb, W):
-#     """
-#     Setup process noise covariance matrix, Q.
-
-#     In general, Q[6:9, 6:9] should be updated each time step if R_nb changes:
-
-#         Q[6:9, 6:9] = dt * (R_nb @ Wv @ R_nb.T)
-
-#     However, if the acceleration noise (velocity random walk) is isotropic (same
-#     in all axes), the rotation is not needed, and we can compute Q only once.
-#     """
-#     # TODO: compute directly, avoiding dfdw computation
-#     dfdw = _wn_input_matrix(R_nb)
-#     Q = dt * dfdw @ W @ dfdw.T
-#     return Q
-
-
-def _wn_cov_matrix(dt, vrw: float, arw: float, gbs: float, gbc: float):
-    Q = np.zeros((9, 9))
-    Q[0:3, 0:3] = dt * arw**2 * np.eye(3)
-    Q[3:6, 3:6] = dt * (2.0 * gbs**2 / gbc) * np.eye(3)
-    Q[6:9, 6:9] = dt * vrw**2 * np.eye(3)
-    return Q
 
 
 @njit  # type: ignore[misc]
