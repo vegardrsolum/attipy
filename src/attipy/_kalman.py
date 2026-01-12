53778656f59a31501df7aecc94f38486eff076d3
Vegard R. Solum <vegard.solum@4subsea.com>
2026-01-12 22:23:59 +0100

    some changes

--- src/attipy/_kalman.py
+++ src/attipy/_kalman.py
@@ -4,7 +4,7 @@ from numpy.typing import NDArray
 
 
 @njit  # type: ignore[misc]
-def _kalman_update(
+def _kalman_update_old(
     x: NDArray[np.float64],
     P: NDArray[np.float64],
     z: NDArray[np.float64],
@@ -30,6 +30,41 @@ def _kalman_update(
 
         # State update
         x += k * (zi - hi @ x)
+
+        # Covariance update (Joseph form)
+        A[:] = I_ - np.outer(k, hi)
+        P[:, :] = A @ P @ A.T + vi * np.outer(k, k)
+    return x, P
+
+
+@njit  # type: ignore[misc]
+def _kalman_update(
+    x: NDArray[np.float64],
+    P: NDArray[np.float64],
+    z: NDArray[np.float64],
+    var: NDArray[np.float64],
+    H: NDArray[np.float64],
+    I_: NDArray[np.float64],
+    Ph: NDArray[np.float64],
+    k: NDArray[np.float64],
+    A: NDArray[np.float64],
+) -> tuple[NDArray[np.float64], NDArray[np.float64]]:
+
+    for i in range(z.shape[0]):
+        hi = H[i]
+        vi = var[i]
+        zi = z[i]
+
+        # Kalman gain
+        for j in range(9):
+            Ph[j] = np.sum(P[j, :] * hi[:])
+        S = np.dot(hi, Ph) + vi
+        if S <= 1e-20:  # numerical safety
+            S = 1e-20
+        k[:] = Ph / S
+
+        # State update
+        x += k * (zi - np.dot(hi, x))
 
         # Covariance update (Joseph form)
         A[:] = I_ - np.outer(k, hi)
