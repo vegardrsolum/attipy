meas matrix fix wrong shape

src/attipy/_statespace.py
@@ -142,7 +142,7 @@ def _dyawda(q_nb: NDArray[np.float64]) -> NDArray[np.float64]:
 
 def _measurement_matrix(q_nb) -> None:
     """Setup linearized measurement matrix, dhdx."""
-    dhdx = np.zeros((7, 9))
+    dhdx = np.zeros((4, 9))
     dhdx[0:3, 6:9] = np.eye(3)  # velocity
     dhdx[3:4, 0:3] = _dyawda(q_nb)  # heading (yaw angle) NB! update each time step
     return dhdx