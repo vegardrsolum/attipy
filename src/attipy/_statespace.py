d5591e2487d6b1d1f3d8e80beac11d155274b015
Vegard R. Solum <vegard.solum@4subsea.com>
2026-01-16 08:27:47 +0100

    small refactor statespace

--- src/attipy/_statespace.py
+++ src/attipy/_statespace.py
@@ -2,7 +2,7 @@ import numpy as np
 from numba import njit
 from numpy.typing import NDArray
 
-from ._vectorops import _skew_symmetric
+from ._vectorops import _skew_symmetric as S
 
 
 def _state_transition(dt, f_b_corr, w_b_corr, R_nb, gbc) -> NDArray[np.float64]:
@@ -12,14 +12,10 @@ def _state_transition(dt, f_b_corr, w_b_corr, R_nb, gbc) -> NDArray[np.float64]:
     First order approximation:
         phi = I + dt * dfdx
     """
-    S = _skew_symmetric  # alias skew symmetric matrix
-
-    beta_gyro = 1.0 / gbc
-
     phi = np.eye(9)
     phi[0:3, 0:3] += -dt * S(w_b_corr)
     phi[0:3, 3:6] += -dt * np.eye(3)
-    phi[3:6, 3:6] += -dt * beta_gyro * np.eye(3)
+    phi[3:6, 3:6] += -dt * np.eye(3) / gbc
     phi[6:9, 0:3] += -dt * (R_nb @ S(f_b_corr))
     return phi
 
@@ -27,7 +23,6 @@ def _state_transition(dt, f_b_corr, w_b_corr, R_nb, gbc) -> NDArray[np.float64]:
 @njit  # type: ignore[misc]
 def _update_state_transition(phi, dt, f_b, w_b, R_nb, I3x3):
     """Update state transition matrix, phi."""
-    S = _skew_symmetric
     phi[0:3, 0:3] = I3x3 - dt * S(w_b)
     phi[6:9, 0:3] = -dt * R_nb @ S(f_b)
 
@@ -46,7 +41,7 @@ def _process_noise_cov(dt, vrw: float, arw: float, gbs: float, gbc: float):
         Q[6:9, 6:9] = dt * (R_nb @ Wv @ R_nb.T)
 
     However, if the acceleration noise (velocity random walk) is isotropic (same
-    for all axes), the rotation is not needed, and we can compute Q only once.
+    in all axes), the rotation is not needed, and we can compute Q only once.
     """
     Q = np.eye(9)
     Q[0:3, 0:3] *= dt * arw**2
@@ -58,31 +53,33 @@ def _process_noise_cov(dt, vrw: float, arw: float, gbs: float, gbc: float):
 def _state_matrix(f_b_corr, w_b_corr, R_nb, gbc) -> NDArray[np.float64]:
     """Setup linearized state matrix, dfdx."""
 
-    beta_gyro = 1.0 / gbc
-
-    S = _skew_symmetric  # alias skew symmetric matrix
-
-    # State transition matrix
     dfdx = np.zeros((9, 9))
     dfdx[0:3, 0:3] = -S(w_b_corr)  # NB! update each time step
     dfdx[0:3, 3:6] = -np.eye(3)
-    dfdx[3:6, 3:6] = -beta_gyro * np.eye(3)
+    dfdx[3:6, 3:6] = -np.eye(3) / gbc
     dfdx[6:9, 0:3] = -R_nb @ S(f_b_corr)  # NB! update each time step
 
     return dfdx
 
 
 def _wn_input_matrix(R_nb: NDArray[np.float64]) -> NDArray[np.float64]:
-    """Setup linearized (white noise) input matrix, dfdw."""
+    """
+    Setup linearized (white noise) input matrix, dfdw.
 
-    # Input (white noise) matrix
+    Notes
+    -----
+    In general, dfdw[6:9, 6:9] should be updated each time step if R_nb changes:
+
+        dfdw[6:9, 6:9] = -R_nb
+
+    However, if the acceleration noise (velocity random walk) is isotropic (same
+    in all axes), the rotation is not needed, and we can compute dfdw only once.
+    """
+
     dfdw = np.zeros((9, 9))
     dfdw[0:3, 0:3] = -np.eye(3)
     dfdw[3:6, 3:6] = np.eye(3)
     dfdw[6:9, 6:9] = -R_nb  # NB! update each time step
-
-    # For isotropic noise, R_nb rotation is not needed, and R_nb can be replaced
-    # with identity matrix. This allows pre-computing dfdw only once.
 
     return dfdw
 
@@ -92,7 +89,6 @@ def _process_noise_psd(
 ) -> NDArray[np.float64]:
     """Setup white noise (process noise) power spectral density matrix, W."""
 
-    # White noise power spectral density matrix
     W = np.eye(9)
     W[0:3, 0:3] *= arw**2
     W[3:6, 3:6] *= 2.0 * gbs**2 / gbc
