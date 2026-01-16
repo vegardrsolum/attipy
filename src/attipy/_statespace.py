3e53d7324d81663b1b972f3cd78a509f03919bd2
Vegard R. Solum <vegard.solum@4subsea.com>
2026-01-16 09:48:57 +0100

    docstring

--- src/attipy/_statespace.py
+++ src/attipy/_statespace.py
@@ -16,13 +16,18 @@ def _state_transition(dt, f_b_corr, w_b_corr, R_nb, gbc) -> NDArray[np.float64]:
     phi[0:3, 0:3] += -dt * S(w_b_corr)
     phi[0:3, 3:6] += -dt * np.eye(3)
     phi[3:6, 3:6] += -dt * np.eye(3) / gbc
-    phi[6:9, 0:3] += -dt * (R_nb @ S(f_b_corr))
+    phi[6:9, 0:3] += -dt * R_nb @ S(f_b_corr)
     return phi
 
 
 @njit  # type: ignore[misc]
 def _update_state_transition(phi, dt, f_b, w_b, R_nb, I3x3):
-    """Update state transition matrix, phi."""
+    """
+    Update state transition matrix, phi.
+
+    Assuming first order approximation:
+        phi = I + dt * dfdx
+    """
     phi[0:3, 0:3] = I3x3 - dt * S(w_b)
     phi[6:9, 0:3] = -dt * R_nb @ S(f_b)
 
@@ -51,49 +56,38 @@ def _process_noise_cov(dt, vrw: float, arw: float, gbs: float, gbc: float):
 
 
 def _state_matrix(f_b_corr, w_b_corr, R_nb, gbc) -> NDArray[np.float64]:
-    """Setup linearized state matrix, dfdx."""
-
+    """
+    Setup linearized state matrix, dfdx.
+    """
     dfdx = np.zeros((9, 9))
     dfdx[0:3, 0:3] = -S(w_b_corr)  # NB! update each time step
     dfdx[0:3, 3:6] = -np.eye(3)
     dfdx[3:6, 3:6] = -np.eye(3) / gbc
     dfdx[6:9, 0:3] = -R_nb @ S(f_b_corr)  # NB! update each time step
-
     return dfdx
 
 
 def _wn_input_matrix(R_nb: NDArray[np.float64]) -> NDArray[np.float64]:
     """
     Setup linearized (white noise) input matrix, dfdw.
-
-    Notes
-    -----
-    In general, dfdw[6:9, 6:9] should be updated each time step if R_nb changes:
-
-        dfdw[6:9, 6:9] = -R_nb
-
-    However, if the acceleration noise (velocity random walk) is isotropic (same
-    in all axes), the rotation is not needed, and we can compute dfdw only once.
     """
-
     dfdw = np.zeros((9, 9))
     dfdw[0:3, 0:3] = -np.eye(3)
     dfdw[3:6, 3:6] = np.eye(3)
     dfdw[6:9, 6:9] = -R_nb  # NB! update each time step
-
     return dfdw
 
 
 def _process_noise_psd(
     vrw: float, arw: float, gbs: float, gbc: float
 ) -> NDArray[np.float64]:
-    """Setup white noise (process noise) power spectral density matrix, W."""
-
+    """
+    Setup white noise (process noise) power spectral density matrix, W.
+    """
     W = np.eye(9)
     W[0:3, 0:3] *= arw**2
     W[3:6, 3:6] *= 2.0 * gbs**2 / gbc
     W[6:9, 6:9] *= vrw**2
-
     return W
 
 
@@ -137,7 +131,9 @@ def _dyawda(q_nb: NDArray[np.float64]) -> NDArray[np.float64]:
 
 
 def _measurement_matrix(q_nb) -> None:
-    """Setup linearized measurement matrix, dhdx."""
+    """
+    Setup linearized measurement matrix, dhdx.
+    """
     dhdx = np.zeros((4, 9))
     dhdx[0:3, 6:9] = np.eye(3)  # velocity
     dhdx[3:4, 0:3] = _dyawda(q_nb)  # heading (yaw angle) NB! update each time step
