b84b106e995592945d459ca941e71b20faca2129
Vegard R. Solum <vegard.solum@4subsea.com>
2026-01-16 09:27:06 +0100

    delete old quatprod

--- src/attipy/_quatops.py
+++ src/attipy/_quatops.py
@@ -25,32 +25,6 @@ def _canonical(q: NDArray[np.float64]) -> NDArray[np.float64]:
     return q
 
 
-# @njit  # type: ignore[misc]
-# def _quatprod(qa: NDArray[np.float64], qb: NDArray[np.float64]) -> NDArray[np.float64]:
-#     """
-#     Unit quaternion (Hamilton) product: q_a ⊗ q_b.
-
-#     Parameters
-#     ----------
-#     qa, qb : numpy.ndarray, shape (4,)
-#         Unit quaternions.
-
-#     Returns
-#     -------
-#     numpy.ndarray, shape (4,)
-#         Unit quaternions result of the product.
-#     """
-#     qa_w, qa_xyz = np.split(qa, [1])
-#     qb_w, qb_xyz = np.split(qb, [1])
-#     return np.concatenate(
-#         (
-#             qa_w * qb_w - qa_xyz.T @ qb_xyz,
-#             qa_w * qb_xyz + qb_w * qa_xyz + np.cross(qa_xyz, qb_xyz),
-#         ),
-#         axis=0,
-#     )
-
-
 @njit  # type: ignore[misc]
 def _quatprod(qa: NDArray[np.float64], qb: NDArray[np.float64]) -> NDArray[np.float64]:
     """
