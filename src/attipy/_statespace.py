d03503913e49e5848ced74fffdf5da18f1316b69
Vegard R. Solum <vegard.solum@4subsea.com>
2026-01-30 19:31:27 +0100

    update state transition

--- src/attipy/_statespace.py
+++ src/attipy/_statespace.py
@@ -37,11 +37,12 @@ def _state_transition(
     phi : ndarray, shape (9, 9)
         State transition matrix.
     """
-    phi = np.eye(9)
-    phi[0:3, 0:3] += -dt * S(w_b)
+    phi = np.eye(12)
     phi[0:3, 3:6] += -dt * np.eye(3)
-    phi[3:6, 3:6] += -dt * np.eye(3) / gbc
-    phi[6:9, 0:3] += -dt * R_nb @ S(f_b)
+    phi[3:6, 6:9] += -dt * R_nb @ S(f_b)
+    phi[6:9, 6:9] += -dt * S(w_b)
+    phi[6:9, 9:12] += -dt * np.eye(3)
+    phi[9:12, 9:12] += -dt * np.eye(3) / gbc
     return phi
 
 
