10540afa7f004c5fafb98cc2fcb5fc17235a3a4f
Vegard R. Solum <vegard.solum@4subsea.com>
2026-02-01 08:05:55 +0100

    fix process noise psd matrix test

--- tests/test_statespace.py
+++ tests/test_statespace.py
@@ -65,9 +65,9 @@ def test_process_noise_psd(noise_params):
 
     # White noise power spectral density matrix
     W = np.eye(9)
-    W[0:3, 0:3] *= arw**2
-    W[3:6, 3:6] *= 2.0 * gbs**2 / gbc
-    W[6:9, 6:9] *= vrw**2
+    W[0:3, 0:3] *= vrw**2
+    W[3:6, 3:6] *= arw**2
+    W[6:9, 6:9] *= 2.0 * gbs**2 / gbc
 
     np.testing.assert_allclose(W_out, W)
 
