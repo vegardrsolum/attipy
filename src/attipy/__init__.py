33f887de8eed2d9244b2c16af922370719e2011c
Vegard R. Solum <vegard.solum@4subsea.com>
2026-02-14 09:11:36 +0100

    delete old mekf

--- src/attipy/__init__.py
+++ src/attipy/__init__.py
@@ -1,6 +1,5 @@
 from ._attitude import Attitude
 from ._mekf import MEKF
-from ._minimekf import MEKF_
 from ._simulate import pva_sim
 
-__all__ = ["Attitude", "MEKF", "MEKF_", "pva_sim"]
+__all__ = ["Attitude", "MEKF", "pva_sim"]
--- src/attipy/_mekf.py
+++ src/attipy/_mekf.py
@@ -1,23 +1,51 @@
 from typing import Self
 
 import numpy as np
+from numba import njit
 from numpy.typing import ArrayLike, NDArray
 
 from ._attitude import Attitude
-from ._kalman import _kalman_update_scalar, _kalman_update_sequential
+from ._kalman import _covariance_update, _kalman_gain
 from ._statespace import (
-    ATT_IDX,
-    BA_IDX,
-    BG_IDX,
-    POS_IDX,
-    VEL_IDX,
-    _dyawda,
     _measurement_matrix,
-    _process_noise_cov,
-    _state_transition,
-    _update_state_transition,
+    _process_noise_cov_matrix,
+    _state_transition_matrix,
+    _update_measurement_matrix_gref,
+    _update_measurement_matrix_yaw,
+    _update_state_transition_matrix,
 )
 from ._transforms import _yaw_from_quat
+from ._vectorops import _normalize_vec
+
+
+def _signed_smallest_angle(angle: float) -> float:
+    """
+    Convert the given angle to the smallest signed angle between [-pi., pi) radians.
+
+    Parameters
+    ----------
+    angle : float
+        Angle in radians.
+
+    Returns
+    -------
+    float
+        The smallest angle between [-pi, pi] radians.
+    """
+    return (angle + np.pi) % (2.0 * np.pi) - np.pi
+
+
+def _z_down(nav_frame) -> NDArray[np.float64]:
+    """
+    Returns +1 if the z-axis of the navigation frame points down (NED), and -1 if
+    it points up (ENU).
+    """
+    if nav_frame.lower() == "ned":
+        return 1
+    elif nav_frame.lower() == "enu":
+        return -1
+    else:
+        raise ValueError(f"Unknown navigation frame: {nav_frame}.")
 
 
 def _gravity_nav(g, nav_frame) -> NDArray[np.float64]:
@@ -45,27 +73,79 @@ def _gravity_nav(g, nav_frame) -> NDArray[np.float64]:
     return g_n
 
 
-def _signed_smallest_angle(angle: float) -> float:
+@njit  # type: ignore[misc]
+def _kalman_update_scalar(da, bg_b, P, z, r, h, I_):
     """
-    Convert the given angle to the smallest signed angle between [-pi., pi) radians.
+    Scalar Kalman filter measurement update.
 
     Parameters
     ----------
-    angle : float
-        Angle in radians.
-
-    Returns
-    -------
-    float
-        The smallest angle between [-pi, pi] radians.
+    da : ndarray, shape (3,)
+        Attitude error estimate to be updated in place.
+    bg_b ndarray, shape (3,)
+        Gyroscope bias estimate to be updated in place.
+    P : ndarray, shape (6, 6)
+        State error covariance matrix to be updated in place.
+    z : float
+        Scalar measurement.
+    r : float
+        Scalar measurement noise variance.
+    h : ndarray, shape (6,)
+        Measurement matrix (row vector).
+    I_ : ndarray, shape (6, 6)
+        Identity matrix.
     """
-    return (angle + np.pi) % (2.0 * np.pi) - np.pi
+
+    # Kalman gain
+    k = _kalman_gain(P, h, r)
+
+    # Updated (a posteriori) state estimate
+    y = z - np.dot(h[0:3], da)
+    da[:] += k[0:3] * y
+    bg_b[:] += k[3:6] * y
+
+    # Updated (a posteriori) covariance estimate (Joseph form)
+    _covariance_update(P, k, h, r, I_)
+
+
+@njit  # type: ignore[misc]
+def _kalman_update_sequential(da, bg_b, P, z, var, H, I_):
+    """
+    Sequential Kalman filter measurement update.
+
+    Parameters
+    ----------
+    da : ndarray, shape (3,)
+        Attitude error estimate to be updated in place.
+    bg_b ndarray, shape (3,)
+        Gyroscope bias estimate to be updated in place.
+    P : ndarray, shape (6, 6)
+        State error covariance matrix to be updated in place.
+    z : ndarray, shape (m,)
+        Measurement vector.
+    var : ndarray, shape (m,)
+        Measurement noise variances corresponding to each scalar measurement.
+    H : ndarray, shape (m, 6)
+        Measurement matrix where each row corresponds to a scalar measurement model.
+    I_ : ndarray, shape (6, 6)
+        Identity matrix.
+    """
+    m = z.shape[0]
+    for i in range(m):
+        _kalman_update_scalar(da, bg_b, P, z[i], var[i], H[i], I_)
+
+
+@njit  # type: ignore[misc]
+def _project_cov_ahead(P, phi, Q):
+    """
+    Project the error covariance ahead.
+    """
+    P[:, :] = phi @ P @ phi.T + Q
 
 
 class MEKF:
     """
-    Multiplicative extended Kalman filter (MEKF) for position, velocity and attitude
-    (PVA) estimation.
+    Multiplicative extended Kalman filter (MEKF) for attitude estimation.
 
     Parameters
     ----------
@@ -74,41 +154,19 @@ class MEKF:
     att : Attitude or array_like, shape (4,)
         Initial attitude estimate as an Attitude instance or a unit quaternion,
         (qw, qx, qy, qz).
-    pos : array_like, shape (3,), default (0.0, 0.0, 0.0)
-        Initial position estimate (px, py, pz) in m expressed in the navigation frame.
-        Defaults to zero position.
-    vel : array_like, shape (3,), default (0.0, 0.0, 0.0)
-        Initial linear velocity estimate (vx, vy, vz) in m/s expressed in the navigation
-        frame. Defaults to zero velocity (stationary).
-    acc : array_like, shape (3,), default (0.0, 0.0, 0.0)
-        Initial linear acceleration estimate (ax, ay, az) in m/s^2 expressed in
-        the navigation frame. Defaults to zero linear acceleration (stationary).
-    ba : array_like, shape (3,), default (0.0, 0.0, 0.0)
-        Initial accelerometer bias estimate (bax, bay, baz) in m/s^2. Defaults to zero bias.
     bg : array_like, shape (3,), default (0.0, 0.0, 0.0)
         Initial gyroscope bias estimate (bgx, bgy, bgz) in rad/s. Defaults to zero bias.
     w : array_like, shape (3,), default (0.0, 0.0, 0.0)
         Initial angular rate estimate (wx, wy, wz) in rad/s expressed in the body frame.
         Defaults to zero angular rate (stationary).
-    P : array_like, shape (15, 15), default 1e-6 * np.eye(15)
+    P : array_like, shape (6, 6), default 1e-6 * np.eye(6)
         Initial error covariance matrix estimate. Defaults to a small diagonal matrix
-        (1e-6 * np.eye(15)). The order of the (error) states is: dx = (dp, dv, da, dba, dbg),
-        where dp is the position error, dv is the velocity error, da is the attitude
-        error (3-parameter 2xGibbs vector), dba is the accelerometer bias error,
-        and dbg is the gyroscope bias error.
-    g : float, default 9.80665
-        The gravitational acceleration. Default is the 'standard gravity' 9.80665.
+        (1e-6 * np.eye(6)). The order of the (error) states is: dx = (da, dbg),
+        where da is the attitude error (3-parameter 2xGibbs vector), and dbg is
+        the gyroscope bias error.
     nav_frame : {'NED', 'ENU'}, default 'NED'
         Specifies the assumed inertial-like navigation frame. Should be 'NED'
         (North-East-Down) (default) or 'ENU' (East-North-Up).
-    acc_noise_density : float, default 0.001
-        Accelerometer noise density (velocity random walk) in (m/s)/√Hz. Defaults
-        to 0.001 (typical value for low-cost MEMS IMUs).
-    acc_bias_stability : float, default 0.0005
-        Accelerometer bias stability (1-sigma) in m/s^2. Defaults to 0.0005 (typical
-        value for low-cost MEMS IMUs).
-    acc_bias_corr_time : float, default 50.0
-        Accelerometer bias correlation time in seconds. Defaults to 50.0 s.
     gyro_noise_density : float, default 0.0001
         Gyroscope noise density (angular random walk) in (rad/s)/√Hz. Defaults to
         0.0001 (typical value for low-cost MEMS IMUs).
@@ -119,24 +177,16 @@ class MEKF:
         Gyroscope bias correlation time in seconds. Defaults to 50.0 s.
     """
 
-    _I15: NDArray[np.float64] = np.eye(15)
+    _I6: NDArray[np.float64] = np.eye(6)
 
     def __init__(
         self,
         fs: float,
         att: Attitude | ArrayLike,
-        pos: ArrayLike = (0.0, 0.0, 0.0),
-        vel: ArrayLike = (0.0, 0.0, 0.0),
-        acc: ArrayLike = (0.0, 0.0, 0.0),
-        ba: ArrayLike = (0.0, 0.0, 0.0),
         bg: ArrayLike = (0.0, 0.0, 0.0),
         w: ArrayLike = (0.0, 0.0, 0.0),
-        P: ArrayLike = 1e-6 * np.eye(15),
-        g: float = 9.80665,
+        P: ArrayLike = 1e-6 * np.eye(6),
         nav_frame: str = "NED",
-        acc_noise_density: float = 0.001,
-        acc_bias_stability: float = 0.0005,
-        acc_bias_corr_time: float = 50.0,
         gyro_noise_density: float = 0.0001,
         gyro_bias_stability: float = 0.00005,
         gyro_bias_corr_time: float = 50.0,
@@ -144,38 +194,28 @@ class MEKF:
         self._fs = fs
         self._dt = 1.0 / fs
         self._nav_frame = nav_frame.lower()
-        self._g = g
-        self._g_n = _gravity_nav(self._g, self._nav_frame)
+        self._z_down = _z_down(self._nav_frame)
 
         # IMU noise parameters
-        self._vrw = acc_noise_density  # velocity random walk
-        self._abs = acc_bias_stability  # accelerometer bias stability
-        self._abc = acc_bias_corr_time  # accelerometer bias correlation time
         self._arw = gyro_noise_density  # angular random walk
         self._gbs = gyro_bias_stability  # gyro bias stability
         self._gbc = gyro_bias_corr_time  # gyro bias correlation time
 
         # State and covariance estimates
         self._att_nb = att if isinstance(att, Attitude) else Attitude(att)
-        self._R_nb = self._att_nb.as_matrix()  # avoiding repeated calls
-        self._p_n = np.asarray_chkfinite(pos).reshape(3).copy()
-        self._v_n = np.asarray_chkfinite(vel).reshape(3).copy()
-        self._a_n = np.asarray_chkfinite(acc).reshape(3).copy()
-        self._ba_b = np.asarray_chkfinite(ba).reshape(3).copy()
         self._bg_b = np.asarray_chkfinite(bg).reshape(3).copy()
-        self._f_b = self._R_nb.T @ (self._a_n - self._g_n)
         self._w_b = np.asarray_chkfinite(w).reshape(3).copy()
-        self._P = np.asarray_chkfinite(P).reshape(15, 15).copy()
-        self._dx = np.zeros(15, dtype=np.float64)
+        self._P = np.asarray_chkfinite(P).reshape(6, 6).copy()
+        self._da = np.zeros(3)  # attitude error state (2xGibbs vector)
 
         # Discretized state space model (updated each time step)
-        self._phi = _state_transition(
-            self._dt, self._f_b, self._w_b, self._R_nb, self._abc, self._gbc
-        )
-        self._Q = _process_noise_cov(
-            self._dt, self._vrw, self._arw, self._abs, self._abc, self._gbs, self._gbc
-        )
-        self._dhdx = _measurement_matrix(self._att_nb._q)
+        self._phi = _state_transition_matrix(self._dt, self._w_b, self._gbc)
+        self._Q = _process_noise_cov_matrix(self._dt, self._arw, self._gbs, self._gbc)
+        self._dhdx = _measurement_matrix(self._att_nb._q, self._vg_b)
+
+    @property
+    def _vg_b(self):
+        return self._z_down * self._att_nb.as_matrix()[2, :]
 
     @property
     def attitude(self) -> Attitude:
@@ -183,39 +223,11 @@ class MEKF:
         return self._att_nb
 
     @property
-    def position(self) -> NDArray[np.float64]:
-        """
-        Copy of the position estimate (m) expressed in the navigation frame.
-        """
-        return self._p_n.copy()
-
-    @property
-    def velocity(self) -> NDArray[np.float64]:
-        """
-        Copy of the linear velocity estimate (m/s) expressed in the navigation frame.
-        """
-        return self._v_n.copy()
-
-    @property
-    def acceleration(self) -> NDArray[np.float64]:
-        """
-        Copy of the linear acceleration estimate (m/s^2) expressed in the navigation frame.
-        """
-        return self._a_n.copy()
-
-    @property
     def bias_gyro(self) -> NDArray[np.float64]:
         """
         Copy of the gyroscope bias estimate (rad/s) expressed in the body frame.
         """
         return self._bg_b.copy()
-
-    @property
-    def bias_acc(self) -> NDArray[np.float64]:
-        """
-        Copy of the accelerometer bias estimate (m/s^2) expressed in the body frame.
-        """
-        return self._ba_b.copy()
 
     @property
     def angular_rate(self) -> NDArray[np.float64]:
@@ -232,69 +244,16 @@ class MEKF:
         """
         return self._P.copy()
 
-    def _dhdx_pos(self):
-        """
-        Position part of the measurement matrix, shape (3, 12).
-        """
-        return self._dhdx[0:3]
-
-    def _dhdx_vel(self):
-        """
-        Velocity part of the measurement matrix, shape (3, 12).
-        """
-        return self._dhdx[3:6]
-
-    def _dhdx_yaw(self, q_nb):
-        """
-        Heading (yaw angle) part of the measurement matrix, shape (12,).
-        """
-        self._dhdx[6:7, ATT_IDX] = _dyawda(q_nb)
-        return self._dhdx[6]
-
     def _reset(self) -> None:
         """
         Reset state (regulating error-state to zero).
         """
 
-        if not self._dx.any():
+        if not self._da.any():
             return
 
-        self._p_n[:] += self._dx[POS_IDX]
-        self._v_n[:] += self._dx[VEL_IDX]
-        self._att_nb._correct_da(self._dx[ATT_IDX])
-        self._ba_b[:] += self._dx[BA_IDX]
-        self._bg_b[:] += self._dx[BG_IDX]
-        self._dx[:] = 0.0
-
-    def _aiding_update_pos(self, pos_meas, pos_var):
-        """
-        Update with position vector aiding measurement.
-        """
-
-        if pos_meas is None:
-            return None
-
-        if pos_var is None:
-            raise ValueError("'pos_var' not provided.")
-
-        dz = pos_meas - self._p_n
-        dhdx = self._dhdx_pos()
-        _kalman_update_sequential(self._dx, self._P, dz, pos_var, dhdx, self._I15)
-
-    def _aiding_update_vel(self, vel_meas, vel_var):
-        """
-        Update with velocity vector aiding measurement.
-        """
-
-        if vel_meas is None:
-            return None
-
-        if vel_var is None:
-            raise ValueError("'vel_var' not provided.")
-
-        dz = vel_meas - self._v_n
-        dhdx = self._dhdx_vel()
-        _kalman_update_sequential(self._dx, self._P, dz, vel_var, dhdx, self._I15)
+        self._att_nb._correct_da(self._da)
+        self._da[:] = 0.0
 
     def _aiding_update_yaw(self, yaw_meas, yaw_var, yaw_degrees):
         """
@@ -311,40 +270,49 @@ class MEKF:
             yaw_meas = (np.pi / 180.0) * yaw_meas
             yaw_var = (np.pi / 180.0) ** 2 * yaw_var
 
-        yaw = _yaw_from_quat(self._att_nb._q)  # heading estimate
-        dz = _signed_smallest_angle(yaw_meas - yaw)
-        dhdx = self._dhdx_yaw(self._att_nb._q)
-        _kalman_update_scalar(self._dx, self._P, dz, yaw_var, dhdx, self._I15)
+        yaw = _yaw_from_quat(self._att_nb._q)
+        z = _signed_smallest_angle(yaw_meas - yaw)
+        dhdx = _update_measurement_matrix_yaw(self._dhdx, self._att_nb._q)
+        _kalman_update_scalar(self._da, self._bg_b, self._P, z, yaw_var, dhdx, self._I6)
+
+    def _aiding_update_gref(self, f_b, gref_var):
+        """
+        Update with gravity reference vector aiding measurement.
+        """
+
+        if f_b is None:
+            return None
+
+        if gref_var is None:
+            raise ValueError("'gref_var' not provided.")
+
+        z = -_normalize_vec(f_b) - self._vg_b
+        dhdx = _update_measurement_matrix_gref(self._dhdx, self._vg_b)
+        _kalman_update_sequential(
+            self._da, self._bg_b, self._P, z, gref_var, dhdx, self._I6
+        )
 
     def _project_ahead(self):
         """
         Project state and covariance estimates ahead.
         """
 
-        # Position (dead reckoning)
-        self._p_n[:] += self._v_n * self._dt
-
-        # Velocity (dead reckoning)
-        self._v_n[:] += self._a_n * self._dt
-
         # Attitude (dead reckoning)
         self._att_nb._project_ahead(self._w_b, self._dt)
 
         # Covariance
-        self._P[:] = self._phi @ self._P @ self._phi.T + self._Q
+        _project_cov_ahead(self._P, self._phi, self._Q)
 
     def update(
         self,
         f: ArrayLike,
         w: ArrayLike,
         degrees: bool = False,
-        pos: ArrayLike | None = (0.0, 0.0, 0.0),
-        pos_var: ArrayLike | None = (1000.0, 1000.0, 1000.0),
-        vel: ArrayLike | None = (0.0, 0.0, 0.0),
-        vel_var: ArrayLike | None = (100.0, 100.0, 100.0),
         yaw: float | None = None,
         yaw_var: float | None = None,
         yaw_degrees: bool = False,
+        gref: bool = True,
+        gref_var: ArrayLike | None = (0.001, 0.001, 0.001),
     ) -> Self:
         """
         Update state estimates with IMU and aiding measurements.
@@ -360,15 +328,6 @@ class MEKF:
         degrees : bool, default False
             Specifies whether the unit of the rotation rate, ``w``, are deg/s
             or rad/s (default).
-        pos : array_like, shape (3,), optional
-            Position measurement (px, py, pz) in m. If ``None``, position aiding is not used.
-        pos_var : array_like, shape (3,), optional
-            Variance of the position measurement noise in m^2. Required for ``pos``.
-        vel : array_like, shape (3,), optional
-            Velocity measurement (vx, vy, vz) in m/s. If ``None``, velocity aiding
-            is not used.
-        vel_var : array_like, shape (3,), optional
-            Variance of the velocity measurement noise in (m/s)^2. Required for ``vel``.
         yaw : float, optional
             Heading (yaw angle) measurement in rad (default) or deg. See ``yaw_degrees``
             for units. If ``None``, heading aiding is not used.
@@ -379,6 +338,11 @@ class MEKF:
         yaw_degrees : bool, default False
             Specifies whether the unit of ``yaw`` and ``yaw_var`` are deg and deg^2
             or rad and rad^2 (default).
+        gref : bool, default True
+            Specifies whether to use the gravity reference vector aiding measurement.
+            If ``False``, gravity reference aiding is not used.
+        gref_var : array_like, shape (3,), default (0.01, 0.01, 0.01)
+            Variance of gravity reference vector measurement noise.
 
         Returns
         -------
@@ -393,18 +357,14 @@ class MEKF:
         self._project_ahead()
 
         # Update (a posteriori) state and covariance estimates with aiding measurements
-        self._aiding_update_pos(pos, pos_var)
-        self._aiding_update_vel(vel, vel_var)
+        self._aiding_update_gref(f if gref else None, gref_var)
         self._aiding_update_yaw(yaw, yaw_var, yaw_degrees)
 
-        # Reset state estimates (regulating error-state to zero)
+        # Reset attitude
         self._reset()
 
         # Update model
-        self._R_nb[:] = self._att_nb.as_matrix()  # avoiding repeated calls
         self._w_b[:] = w - self._bg_b
-        self._f_b[:] = f - self._ba_b
-        self._a_n[:] = self._R_nb @ self._f_b + self._g_n
-        _update_state_transition(self._phi, self._dt, self._f_b, self._w_b, self._R_nb)
+        _update_state_transition_matrix(self._phi, self._dt, self._w_b)
 
         return self
