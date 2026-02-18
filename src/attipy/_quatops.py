7cf83281507676d3026c7a174206dcaecb145a52
Vegard R. Solum <vegard.solum@4subsea.com>
2026-02-18 10:04:34 +0100

    black

--- src/attipy/_quatops.py
+++ src/attipy/_quatops.py
@@ -84,9 +84,7 @@ def _normalize(q: NDArray[np.float64]) -> NDArray[np.float64]:
 
 
 @njit  # type: ignore[misc]
-def _correct_quat_with_gibbs2(
-    q: NDArray[np.float64], da: NDArray[np.float64]
-) -> None:
+def _correct_quat_with_gibbs2(q: NDArray[np.float64], da: NDArray[np.float64]) -> None:
     """
     Corrects a unit quaternion, q, with a small attitude error, da, parameterized
     as a scaled (2x) Gibbs vector.
