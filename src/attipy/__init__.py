a759e69135f49455b91235eca2049f5b60db5033
Vegard R. Solum <vegard.solum@4subsea.com>
2026-02-20 14:58:31 +0100

    rename to mekf

--- src/attipy/__init__.py
+++ src/attipy/__init__.py
@@ -1,5 +1,5 @@
 from ._attitude import Attitude
-from ._mekf import AttMEKF
+from ._mekf import MEKF
 from ._simulate import pva_sim
 
-__all__ = ["Attitude", "AttMEKF", "pva_sim"]
+__all__ = ["Attitude", "MEKF", "pva_sim"]
