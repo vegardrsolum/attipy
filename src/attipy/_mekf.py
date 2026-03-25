5303786168c3a8d0d143c26ab54218f06506b46b
Vegard R. Solum <vegard.solum@4subsea.com>
2026-03-25 21:48:52 +0100

    simplify reset

--- src/attipy/_mekf.py
+++ src/attipy/_mekf.py
@@ -119,8 +119,7 @@ def _reset(q_nb, bg_b, dx) -> None:
     bg_b[0] += dx[3]
     bg_b[1] += dx[4]
     bg_b[2] += dx[5]
-    for i in range(6):
-        dx[i] = 0.0
+    dx[:] = 0.0
 
 
 class MEKF:
