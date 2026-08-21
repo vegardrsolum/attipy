a0d600450ed3abf3a7a5d60d34ed708ad8d7d74f
Vegard R. Solum <vegard.solum@4subsea.com>
2026-08-21 11:20:46 +0200

    more code

--- src/attipy/_smoothing.py
+++ src/attipy/_smoothing.py
@@ -6,8 +6,17 @@ class RTSSmoother:
     def __init__(self, mekf):
         self._mekf = mekf
 
+        self._q_buf = []
+        self._b_buf = []
+        self._P_buf = []
+        self._dtheta_buf = []
+
     def update(self, *args, **kwargs):
         """
         Update with IMU and aiding measurements.
         """
         self._mekf.update(*args, **kwargs)
+        self._q_buf.append(self._mekf.attitude.as_quaternion())
+        self._b_buf.append(self._mekf.bias)
+        self._P_buf.append(self._mekf.P)
+        self._dtheta_buf.append(self._mekf._dtheta.copy())
