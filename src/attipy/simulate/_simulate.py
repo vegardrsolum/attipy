1657e5001ed8d76c724fc270215d1b24e154f77d
Vegard R. Solum <vegard.solum@4subsea.com>
2026-09-23 10:24:44 +0200

    docstring fix

--- src/attipy/simulate/_simulate.py
+++ src/attipy/simulate/_simulate.py
@@ -502,12 +502,12 @@ def trajectory(
     nav_frame : {'NED', 'ENU'}, optional
         Specifies the navigation frame. Either 'NED' (North-East-Down) or 'ENU'
         (East-North-Up). Defaults to 'NED'.
-    motion_type : {'beat-6dof', 'beat-3dof', 'stationary'} or MotionType, optional
+    motion_type : str or MotionType, optional
         Specifies the type of motion to generate. Should a string specifying one
         of the predefined types of motion:
         - 'beat-6dof': Beating sinusoidal motion in all six degrees of freedom.
         - 'beat-3dof': Beating sinusoidal motion in roll, pitch and yaw only.
-        - 'stationary': Standstill motion.
+        - 'stationary': No motion; all degrees of freedom remain constant (zero).
         or a custom ``MotionType`` instance.
 
     Returns
