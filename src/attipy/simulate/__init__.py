b8e2a85c8fe826f5f0a88adec0c3deba5e889e29
Vegard R. Solum <vegard.solum@4subsea.com>
2026-09-25 07:51:51 +0200

    split simulate module into _dof and _simulate

--- src/attipy/simulate/__init__.py
+++ src/attipy/simulate/__init__.py
@@ -1,3 +1,4 @@
-from ._simulate import DOF, BeatDOF, ConstantDOF, Motion, RampUp, trajectory
+from ._dof import DOF, BeatDOF, ConstantDOF, RampUp
+from ._simulate import Motion, trajectory
 
 __all__ = ["DOF", "BeatDOF", "ConstantDOF", "Motion", "RampUp", "trajectory"]
