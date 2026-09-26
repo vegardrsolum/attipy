ae79887715679b8d8d8c421672f229e11ac0dd32
vegardrsolum <41550787+vegardrsolum@users.noreply.github.com>
2026-09-26 06:29:24 +0200

    enh: simulate sinusoidal DOF (#108)

--- src/attipy/simulate/__init__.py
+++ src/attipy/simulate/__init__.py
@@ -1,4 +1,12 @@
-from ._dof import DOF, BeatDOF, ConstantDOF, RampUp
+from ._dof import DOF, BeatDOF, ConstantDOF, RampUp, SineDOF
 from ._simulate import Motion, trajectory
 
-__all__ = ["DOF", "BeatDOF", "ConstantDOF", "Motion", "RampUp", "trajectory"]
+__all__ = [
+    "DOF",
+    "BeatDOF",
+    "ConstantDOF",
+    "Motion",
+    "RampUp",
+    "SineDOF",
+    "trajectory",
+]
